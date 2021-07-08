46ce953155eaf2c04081680146462ccfd1b13f4a
IanJames2 <ianjms81@gmail.com>
2021-07-07 22:24:51 -0500

    Finished Listing Animals from the DB. Need to revisit the code and look through it one more time before moving on.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
+import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalCard } from "./animal/AnimalCard"
+import { AnimalList } from "./animal/AnimalList"
 import { EmployeesCard } from "./employees/EmployeesCard"
 import { LocationsCard } from "./locations/LocationsCard"
 import { CustomersCard } from "./customers/CustomersCard"
@@ -9,28 +11,29 @@ import { CustomersCard } from "./customers/CustomersCard"
 
 export const ApplicationViews = () => {
     return (
-        <>
-            {/* Render the location list when http://localhost:3000/ */}
-            <Route exact path="/">
-                <Home />
-            </Route>
+      <>
+        {/* Render the location list when http://localhost:3000/ */}
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-            {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+        <AnimalProvider>
+          <Route exact path="/animals">
+            <AnimalList />
+          </Route>
+        </AnimalProvider>
 
-            <Route path="/locations">
-                <LocationsCard />
-            </Route>
+        <Route path="/locations">
+          <LocationsCard />
+        </Route>
 
-            <Route path="/customers">
-                <CustomersCard />
-            </Route>
+        <Route path="/customers">
+          <CustomersCard />
+        </Route>
 
-            <Route path="/employees">
-                <EmployeesCard />
-            </Route>
-        </>
-    )
+        <Route path="/employees">
+          <EmployeesCard />
+        </Route>
+      </>
+    );
 }
