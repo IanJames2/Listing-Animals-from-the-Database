76579d7681ddcf865bcac4bfd4020556f2e79815
IanJames2 <ianjms81@gmail.com>
2021-07-09 22:03:39 -0500

    Got the data to render using expand in my query selector for the fetch call to the db. I understand expand now and what it can do. Connections one array of objects to another array of objs to access other properties.

---
+++ src/components/customers/CustomerProvider.js
@@ -0,0 +1,40 @@
+import React, { useState, createContext } from "react"
+
+// The context is imported and used by individual components that need data
+export const CustomerContext = createContext()
+
+// This component establishes what data can be used.
+export const CustomerProvider = (props) => {
+    const [customers, setcustomers] = useState([])
+
+    const getCustomers = () => {
+        return fetch("http://localhost:8088/customers?_expand=animal")
+        .then(res => res.json())
+        .then(setcustomers)
+    }
+
+    const addCustomer = employeeObj => {
+        return fetch("http://localhost:8088/customers", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(employeeObj)
+        })
+        .then(getCustomers)
+    }
+
+    /*
+        You return a context provider which has the
+        `customers` state, `getCustomers` function,
+        and the `addCustomer` function as keys. This
+        allows any child elements to access them.
+    */
+    return (
+        <CustomerContext.Provider value={{
+            customers, getCustomers, addCustomer
+        }}>
+            {props.children}
+        </CustomerContext.Provider>
+    )
+}
--- src/components/employees/EmployeeCard.js
+++ src/components/employees/EmployeeCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 import "./Employees.css"
 
 
-export const EmployeesCard = () => (
+export const EmployeeCard = ({employee}) => (
     <section className="employee">
         <h3 className="employee__name">Emma Beaton</h3>
-        <div className="employee__breed">Nashville Kennels North</div>
+        <div className="employee__location">{employee.location.name}</div>
     </section>
     )
 
